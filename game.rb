3d7fcce201f08c69e729ee441506a5e8059064cf
Alexander Blythe <Alexander.blythe@gmail.com>
2015-02-04 15:15:28 -0500

    Added scoring

--- game.rb
+++ game.rb
@@ -1,35 +1,52 @@
 class Game
-  attr_reader :name, :players
+  attr_reader :name, :players, :scores, :round, :turn_score
   def initialize
     @name = nil
     @players =[]
+    @scores = {}
+    @round = 0
+    @turn_score = 0
   end
-
 
   def get_players
     puts "How many players?"
     number_of_players = gets.chomp.to_i
     count = 1
-    number_of_players.times do |player|
+    number_of_players.times do
       puts "Enter name for player #{count}: "
       p = gets.chomp
       @players.push p
+      @scores[p] = 0
       count += 1
+      puts
     end
+  end
+
+  def update_score 
+    @scores[turn?] += @turn_score
+  end
+
+
+  def turn?
+    n =@round % players.count
+    @players[n]
+  end
+
+  def change_turn
+    @round += 1
+    @turn_score =0
   end
 
 end
 
-
 class Hog < Game
-  attr_reader :dice, :players, :score
+  attr_reader :dice
   def initialize *players
+    super
     @name = "Hog"
     @dice = nil
     @players = *players
-    @score = 0
     @last_roll =0
-    @round=0
   end
 
   def how_many_dice n 
@@ -40,20 +57,9 @@ class Hog < Game
     @dice.times do
       @last_roll = test ||rand(1..6)
       unless @last_roll == 1
-        @score += @last_roll
+        @turn_score += @last_roll
       end
-      @round += 1
       @last_roll
     end
   end
-
-  def turn
-    @round % @players.count
-  end
-
-  def turn?
-    @players[turn]
-  end
-
-
 end
--- game_player.rb
+++ game_player.rb
@@ -1,7 +1,23 @@
 require './game'
 
 puts "Welcome to Hog."
-g= Game.new
-g.get_players
+h= Hog.new
+h.get_players
+
+count = 1
+h.players.each do |player|
+  puts "Player #{count}: #{player}"
+  count += 1
+end
+puts
+puts "#{h.turn?}'s turn"
+
+puts "How many dice do you want to roll? "
+h.how_many_dice gets.chomp.to_i
+
+puts "Rolling #{h.dice} dice"
+h.roll
+h.update_score
+puts "You got #{h.turn_score}, your total is now: #{h.scores[h.turn?]}"
 
 
