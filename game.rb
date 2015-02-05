5876e8ad85d70a89e7da4a6a6aae8bc32c32a139
Alexander Blythe <Alexander.blythe@gmail.com>
2015-02-05 16:31:14 -0500

    Working Hog and Pig game

--- game.rb
+++ game.rb
@@ -1,9 +1,10 @@
 class Game
-  attr_reader :name, :players, :scores, :round, 
-  :turn_score, :winner, :done_playing
-  def initialize
+  attr_reader :name, :scores, :round, :players,
+  :turn_score, :winner, :done_playing, :turn_score, :number_of_players
+  
+  def initialize 
     @name = nil
-    @players =[]
+    @players = []
     @scores = {}
     @round = 0
     @turn_score = 0
@@ -11,20 +12,22 @@ class Game
     @winner = ""
     @max =20
     @done_playing = false
+    @turn_over = false
   end
 
-  def get_players
-    puts "How many players?"
-    number_of_players = gets.chomp.to_i
-    count = 1
-    number_of_players.times do
-      puts "Enter name for player #{count}: "
-      p = gets.chomp
-      @players.push p
-      @scores[p] = 0
-      count += 1
-      puts
+  def test_players *names
+    names.each do |player|
+      @players.push player
     end
+  end
+
+  def how_many_players number
+    @number_of_players = number
+  end
+  
+  def add_players player
+    @players.push player
+    @scores[player] = 0
   end
 
   def update_score 
@@ -36,14 +39,22 @@ class Game
   end
 
   def turn?
-    n =@round % players.count
+    n =@round % @players.count
     @players[n]
+  end
+
+  def turn_over
+    @turn_over
+  end
+
+  def end_turn
+    @turn_over =true
   end
 
   def change_turn
     @round += 1
     @turn_score =0
-    puts "-" * 50
+    @turn_over =false
   end
 
   def won?
@@ -62,42 +73,72 @@ class Game
     puts "Pick a game to play:"
     puts "1) Pig"
     puts "2) Hog"
-    choice = if gets.chomp.to_i == 1
-      Pig
+    choice = nil
+    if gets.chomp.to_i == 1
+      choice = Pig
     else
-      Hog
+      choice =Hog
     end
-    choice.new
+    choice
   end
 
 end
 
 class Hog < Game
   attr_reader :dice
-  def initialize *players
+  def initialize
     super
     @name = "Hog"
-    @dice = nil
-    @players = *players
-    @last_roll =0
-    puts "Welcome to Hog."
   end
 
-  def how_many_dice n 
-    @dice = n
-  end
 
-  def roll 
-    @turn_score = 0
-    @dice.times do
+  def roll number
+    number.times do
       value = rand(1..6)
       if value == 1
         @turn_score = 0
-        puts "You rolled a one!"
-        return 
+        @turn_over = true
+        return true
       else
         @turn_score += value
+        
       end
     end
+    @turn_over = true
+    return false
   end
+
+  def take_turn
+    @turn_score = 0
+    puts "How many dice do you want to roll? "
+    number= gets.chomp.to_i
+    puts
+    puts "Rolling #{number} dice"
+    roll number
+  end
+
+end
+
+class Pig < Game
+  def initialize 
+    super
+    @name = "Pig"
+  end
+
+  def take_turn
+    roll
+  end
+
+  def roll
+    roll = rand(1..6)
+    if roll == 1
+      @turn_score = 0
+      @turn_over = true
+      return true
+    end
+    @turn_score += roll
+    return false
+  end
+
+
 end
--- game_player.rb
+++ game_player.rb
@@ -1,9 +1,19 @@
 require './game'
 g=Game.new
+
 until g.done_playing
-  game =g.pick_game
+  game =g.pick_game.new
+  puts "Welcome to #{game.name}."
   puts
-  game.get_players
+  puts "How many players?"
+  game.how_many_players gets.chomp.to_i
+  puts
+  count = 1
+  players =[]
+  game.number_of_players.times do
+    puts "Enter name for player #{count}: "
+    game.add_players gets.chomp
+  end
 
   count = 1
   game.players.each do |player|
@@ -11,21 +21,35 @@ until g.done_playing
     count += 1
   end
   puts
+
   until game.won? 
     puts "#{game.turn?}'s turn"
-    puts "How many dice do you want to roll? "
-    game.how_many_dice gets.chomp.to_i
-    puts
-    puts "Rolling #{game.dice} dice"
-    game.roll
-    sleep 0.5
-    puts
+    until game.turn_over
+      sleep 0.5
+      got_one = false
+      got_one = game.take_turn
+      if got_one
+        puts "You rolled a 1!."
+      end
+      puts "Your turn score is #{game.turn_score}." 
+      puts
+      if game.turn_over == false
+        puts "Roll again? y/n"
+        input = gets.chomp
+        if input == "n"
+          game.end_turn
+        end
+      end
+    end
+
     game.update_score
     puts "You got #{game.turn_score}, your total is now: #{game.scores[game.turn?]}"
     game.change_turn
+    puts "-" * 50
     sleep 0.5
     puts
   end
+
   puts "Game Over. #{game.winner} wins!"
   puts "Final scores: "
   game.scores.each { |p, s| puts "#{p}: #{s}"}
